7d15ab5b753b1aa448b8636145f7041f67fca45d
Dragos Buhnila <s331394@studenti.polito.it>
2024-10-26 19:07:49 +0200

    Implemented the Document and DocumentLink objects, as well as DocumentType and LinkType enums.

---
+++ server/notes_for_everybody.md
@@ -0,0 +1,6 @@
+#### Use this document to add things that everybody should take into account when developing their code. If you have misgivings or doubts contact the author of the comment.  
+
+
+# Dates  
+(Dragos)  
+Every time a field requires the date type use the `dayjs` date type instead of javascript's `Date`.  
--- server/src/models/document.ts
+++ server/src/models/document.ts
@@ -1,1 +1,97 @@
-// export default Document;
+import { Dayjs } from "dayjs";
+
+enum DocumentType {
+    Informative = "INFORMATIVE",
+    Prescriptive = "PRESCRIPTIVE",
+    Design = "DESIGN",
+    Technical = "TECHNICAL",
+    MaterialEffect = "MATERIAL_EFFECT",
+}
+
+class Document {
+    id: number;
+    title: string;
+
+    issuanceDate?: Dayjs;
+    language?: string;
+    pages?: number;
+
+    stakeholders?: string;
+    scale?: string;
+    description?: string;
+    type: DocumentType;
+
+    coordinates?: string;
+    lastModifiedBy: string;
+
+    constructor(id: number, title: string, type: DocumentType, lastModifiedBy: string,  // Required fields
+                issuanceDate?: Dayjs, language?: string, pages?: number,                 // Optional fields
+                stakeholders?: string, scale?: string,
+                description?: string, coordinates?: string) {
+        this.id = id;
+        this.title = title;
+        this.issuanceDate = issuanceDate;
+        this.language = language;
+        this.pages = pages;
+        this.stakeholders = stakeholders;
+        this.scale = scale;
+        this.description = description;
+        this.type = type;
+        this.coordinates = coordinates;
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    // Setters
+    setIssuanceDate(issuanceDate: Dayjs) {
+        this.issuanceDate = issuanceDate;
+    }
+
+    setLanguage(language: string) {
+        this.language = language;
+    }
+
+    setPages(pages: number) {
+        this.pages = pages;
+    }
+
+    setStakeholders(stakeholders: string) {
+        this.stakeholders = stakeholders;
+    }
+
+    setScale(scale: string) {
+        this.scale = scale;
+    }
+
+    setDescription(description: string) {
+        this.description = description;
+    }
+
+    setCoordinates(coordinates: string) {
+        this.coordinates = coordinates;
+    }
+}
+
+enum LinkType {
+    Direct = "DIRECT",
+    Collateral = "COLLATERAL",
+    Projection = "PROJECTION",
+    Update = "UPDATE",
+}
+
+class DocumentLink {
+    linkId: number;
+    docId1: number;
+    docId2: number;
+    linkType: LinkType;
+    createdAt: Dayjs;
+
+    constructor(linkId: number, docId1: number, docId2: number, linkType: LinkType, createdAt: Dayjs) {
+        this.linkId = linkId;
+        this.docId1 = docId1;
+        this.docId2 = docId2;
+        this.linkType = linkType;
+        this.createdAt = createdAt;
+    }
+}
+
+export { Document, DocumentType, DocumentLink, LinkType };
