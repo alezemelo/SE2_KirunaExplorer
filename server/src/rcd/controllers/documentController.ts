7528ee10490f31ce5d10ef6f94f9dafab6f77e9f
Dragos Buhnila <s331394@studenti.polito.it>
2024-10-30 18:43:46 +0100

    Implemented Routes and Controllers for Get Doc and Update Doc Description

---
+++ server/src/rcd/controllers/documentController.ts
@@ -0,0 +1,60 @@
+import { DocumentNotFoundError } from "../../errors/documentErrors";
+import DocumentDAO from "../daos/documentDAO";
+import { Document } from "../../models/document";
+
+/**
+ * Represents a controller for handling document-related operations.
+ */
+class DocumentController {
+    private dao: DocumentDAO;
+
+    /**
+     * Constructs a new instance of the DocumentController class.
+     */
+    constructor() {
+        this.dao = new DocumentDAO();
+    }
+
+    /**
+     * Updates the description of a document.
+     * 
+     * @param id - The ID of the document to update.
+     * @param description - The new description for the document.
+     * @throws DocumentNotFoundError if the document with the specified ID is not found.
+     * @throws Error if more than one document was updated or for other generic errors.
+     */
+    async updateDescription(id: number, description: string): Promise<void> {
+        try {
+            const amount_updated = await this.dao.updateDescription(id, description);
+            if (amount_updated === 0) {
+                throw new DocumentNotFoundError([id]);
+            } else if (amount_updated > 1) {
+                throw new Error("More than one document was updated.");
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
+    * Retrieves a document by its ID.
+    * 
+    * @param id - The ID of the document to retrieve.
+    * @returns The document with the specified ID.
+    * @throws DocumentNotFoundError if the document with the specified ID is not found.
+    * @throws Error for generic errors.
+    */
+    async getDocument(id: number): Promise<Document> {
+        try {
+            const doc = await this.dao.getDocument(id);
+            if (doc === null) {
+                throw new DocumentNotFoundError([id]);
+            }
+            return doc;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default DocumentController;
