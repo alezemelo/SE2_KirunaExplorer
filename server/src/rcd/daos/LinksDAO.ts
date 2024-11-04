7c6f8868aa89a96bf987adeff694613ca2b5fc65
Lorenzo Gaetani s333048 <s333048@studenti.polito.it>
2024-11-04 18:48:42 +0100

    fix

--- server/src/rcd/daos/LinksDAO.ts
+++ server/src/rcd/daos/LinksDAO.ts
@@ -1,4 +1,4 @@
-import { pool } from "../../db/db";
+import pgdb from "../../db/temp_db";
 import { DocumentLink, LinkType } from "../../models/document";
 import dayjs from "dayjs";
 
@@ -9,7 +9,7 @@ class LinksDAO{
     /*async getLink(link_id: number): Promise<DocumentLink>{
         try {
             const sql = "SELECT * FROM document_links WHERE link_id=?";
-            const result = await pool.query(sql,[link_id]);
+            const result = await pgdb.client.query(sql,[link_id]);
             return result.rows[0];
         } catch(error){
             throw error; 
@@ -19,7 +19,7 @@ class LinksDAO{
     async getLinks(id: number): Promise<DocumentLink[]>{
         try {
             const sql = "SELECT * FROM document_links WHERE doc_id1=$1 OR doc_id2=$1";
-            const result = await pool.query(sql,[id]);
+            const result = await pgdb.client.query(sql,[id]);
             const res = result.rows.map(row => new DocumentLink(
                 row.link_id,
                 row.doc_id1,
@@ -36,12 +36,12 @@ class LinksDAO{
     async createLink(doc_id1:number,doc_id2:number,link_type:LinkType): Promise<any>{
         try {
             const sql1 = "SELECT * FROM document_links WHERE ((doc_id1 = $1 AND doc_id2 = $2) OR (doc_id1 = $2 AND doc_id2 = $1)) AND link_type = $3";
-            const result = await pool.query(sql1,[doc_id1,doc_id2,link_type]);
+            const result = await pgdb.client.query(sql1,[doc_id1,doc_id2,link_type]);
             if(result.rows.length>0){
                 throw new Error("link already exists");
             }
             const sql = "INSERT INTO document_links (doc_id1,doc_id2,link_type) VALUES($1,$2,$3)";
-            const res = await pool.query(sql,[doc_id1,doc_id2,link_type]);
+            const res = await pgdb.client.query(sql,[doc_id1,doc_id2,link_type]);
             return res.rowCount;
         } catch(error){
             throw error; 
