ecd3e6875624000cd3eaeb709b63ba373b1730f9
Alessandro <alezemelo@gmail.com>
2024-12-02 15:45:32 +0100

    SonarCloud setting

--- server/jest.config.js
+++ server/jest.config.js
@@ -6,4 +6,7 @@ module.exports = {
   },
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true, // Enable coverage collection
+  coverageDirectory: 'coverage', // Output directory for coverage reports
+  coverageReporters: ['lcov', 'json-summary', 'text'], // Coverage formats for SonarCloud
 };
