77459008c86703c3328d96aebe7f1038f8eaa152
Dragos Buhnila <s331394@studenti.polito.it>
2024-12-09 16:19:16 +0100

    td for landing page upate

--- client/src/components/TimeDiagram/TimeDiagram.tsx
+++ client/src/components/TimeDiagram/TimeDiagram.tsx
@@ -5,8 +5,7 @@ import { Box, Typography } from '@mui/material';
 const TimeDiagramPage: React.FC = () => {
   return (
     <Box sx={{ padding: 4 }}>
-      <Typography variant="h4">Time Diagram Page</Typography>
-      <Typography variant="body1">This page is under development.</Typography>
+      <Typography variant="h4">Time Diagram Page Placeholder. Override everything if merge cocnflict arise once the actual page is done</Typography>
     </Box>
   );
 };
