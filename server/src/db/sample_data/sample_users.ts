e309248d43ba0f68acfe239e0f2cb928b524c55d
Angelo Bongiorno <s331164@studenti.polito.it>
2025-01-09 15:13:42 +0100

    new user added

--- server/src/db/sample_data/sample_users.ts
+++ server/src/db/sample_data/sample_users.ts
@@ -17,5 +17,11 @@ export const SAMPLE_USERS = [
         hash: "d5a43cfbed4ef660d6449649c2599a35ecd1dcd74d67538a5695fbcb43d2dc8e",
         salt: "610dbce01d172d82fa4ad2d1005dfc5d",
         type: "urban_planner",
+    },
+    {
+      username: "FrejaJohansson",
+      hash: "1998faad3d7a353abe8f4c9297827760a1e2d1d8394e34b2de6908cf0a9f5166",
+      salt: "20d11a9d7eb12a22b15fb53c23974cf2",
+      type: "urban_planner"
     }
 ];
